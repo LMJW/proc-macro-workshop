53945dbabe5a788b25dbb51b3bba1453aa090c11
LMJW <heysuperming@gmail.com>
2020-03-10 00:24:01 +1100

    Pass test 5 (seq macro)

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -10,30 +10,53 @@ use quote::quote;
 use std::iter::FromIterator;
 
 use syn::parse::{Parse, ParseStream};
-use syn::{braced, parse_macro_input, Result, Token};
+use syn::{braced, parenthesized, parse_macro_input, Result, Token};
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as SeqMacroInput);
 
-    let ident = input.ident;
+    // let ident = input.ident;
     let starti = input.starti;
     let endi = input.endi;
-    let fnbody = input.fnbody;
+    // let fnbody = input.fnbody;
+    let enumbody = input.enumbody;
 
     let a = starti.base10_parse::<u64>().unwrap();
     let b = endi.base10_parse::<u64>().unwrap();
 
-    let fns: Vec<_> = (a..b)
-        .into_iter()
-        .map(|n| expand(&fnbody, n))
-        .collect();
+    if enumbody.expand_section.is_some() {
+        // exps : expands
+        let exps: Vec<_> = (a..b)
+            .into_iter()
+            .map(|n| expand_enum(&enumbody, n))
+            .collect();
+        let dm = if let Some(mac) = enumbody.attrs {
+            let punct = mac.punct.clone();
+            let group = mac.group.clone();
+            quote! {
+                #punct#group
+            }
+        } else {
+            quote! {}
+        };
+        // eprintln!("????{:#?}", dm);
 
-    let ret = quote! {
-        #(#fns)*
-    };
+        let eident = enumbody.ename.clone();
+        // eprintln!("????{:#?}", eident);
 
-    ret.into()
+        let ret = quote! {
+            #dm
+            enum #eident{
+                #(#exps)*
+            }
+        };
+
+        // eprintln!("????{:#?}", ret);
+        ret.into()
+    } else {
+        unimplemented!()
+    }
     // proc_macro::TokenStream::new()
 }
 
@@ -46,7 +69,8 @@ struct SeqMacroInput {
     // dottok: Token![..],
     endi: syn::LitInt,
     // block: syn::token::Brace,
-    fnbody: FnContent,
+    // fnbody: FnContent,
+    enumbody: EnumContent,
 }
 
 #[derive(Debug)]
@@ -57,6 +81,85 @@ struct FnContent {
     replace2: proc_macro2::Ident,
     op: Token![*],
     lit: syn::LitInt,
+}
+
+#[derive(Debug, Clone)]
+struct DeriveMacro {
+    punct: proc_macro2::Punct,
+    group: proc_macro2::Group,
+}
+
+#[derive(Debug, Clone)]
+struct EnumContent {
+    attrs: Option<DeriveMacro>,
+    ename: proc_macro2::Ident,
+    expand_section: Option<ReplaceSection>,
+}
+
+#[derive(Debug, Clone)]
+struct ReplaceSection {
+    ident: proc_macro2::Ident,
+    repl: proc_macro2::Ident,
+    punct: proc_macro2::Punct,
+}
+
+impl Parse for DeriveMacro {
+    fn parse(input: ParseStream) -> Result<Self> {
+        Ok(Self {
+            punct: input.parse()?,
+            group: input.parse()?,
+        })
+    }
+}
+
+impl Parse for EnumContent {
+    fn parse(input: ParseStream) -> Result<Self> {
+        // eprintln!(">>>>>{:#?}", input);
+        let attrs = if input.peek(Token![#]) && input.peek2(syn::token::Bracket) {
+            // parse the derive macro if exists
+            let res = input.call(DeriveMacro::parse)?;
+
+            Some(res)
+        } else {
+            None
+        };
+        // parse the enum struct
+
+        let _x: Token![enum] = input.parse()?;
+
+        let ename: proc_macro2::Ident = input.parse()?;
+        let body;
+
+        let _: syn::token::Brace = braced!(body in input);
+
+        let expand_section = if body.peek(Token![#]) && body.peek2(syn::token::Paren) {
+            let _: proc_macro2::Punct = body.parse()?; //# token
+            let content;
+            let _: syn::token::Paren = parenthesized!(content in body);
+            let repl = content.call(ReplaceSection::parse)?;
+            let _: proc_macro2::Punct = body.parse()?; //* token
+            Some(repl)
+        } else {
+            None
+        };
+
+        Ok(Self {
+            attrs,
+            ename,
+            expand_section,
+        })
+    }
+}
+
+impl Parse for ReplaceSection {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let ident: proc_macro2::Ident = input.parse()?;
+        let _: proc_macro2::Punct = input.parse()?;
+        let repl: proc_macro2::Ident = input.parse()?;
+        let punct: proc_macro2::Punct = input.parse()?;
+
+        Ok(Self { ident, repl, punct })
+    }
 }
 
 impl Parse for FnContent {
@@ -98,10 +201,10 @@ impl Parse for SeqMacroInput {
         let endi: syn::LitInt = input.parse()?; //4
 
         let content;
-        let mut block: syn::token::Brace = braced!(content in input);
+        let _: syn::token::Brace = braced!(content in input);
 
-        let res = content.call(FnContent::parse).unwrap();
-        eprintln!("---{:#?}", res);
+        let res = content.call(EnumContent::parse).unwrap();
+        // eprintln!("---{:#?}", res);
 
         let ret = SeqMacroInput {
             ident,
@@ -110,24 +213,38 @@ impl Parse for SeqMacroInput {
             // dottok,
             endi,
             // block,
-            fnbody: res,
+            enumbody: res,
         };
         Ok(ret)
+    }
+}
+
+fn expand_enum(input: &EnumContent, n: u64) -> proc_macro2::TokenStream {
+    if let Some(esec) = input.expand_section.clone() {
+        let id = esec.ident.to_string();
+        let new_id = format!("{}{}", id, n);
+        let nident = proc_macro2::Ident::new(new_id.as_str(), esec.ident.span());
+
+        quote! {
+            #nident,
+        }
+    } else {
+        unimplemented!()
     }
 }
 
 // expand the stmt expression
 fn expand(input: &FnContent, n: u64) -> proc_macro2::TokenStream {
     let fname = input.fname.to_string();
-    let nfname = format!("{}{}",fname, n);
+    let nfname = format!("{}{}", fname, n);
     let ret = input.ret.clone();
     let op = input.op.clone();
     let lit = input.lit.clone();
 
     let nfname = proc_macro2::Ident::new(nfname.as_str(), input.fname.span());
     let nlit = proc_macro2::Literal::u64_unsuffixed(n);
-    
-    let ret = quote!{
+
+    let ret = quote! {
         fn #nfname() -> #ret{
             #nlit #op #lit
         }
